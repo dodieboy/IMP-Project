469dfba3765f93e317ca9f5fb16d2fa7d20f67ff
dodieboy <dodieboy@users.noreply.github.com>
2019-07-12 12:04:57 +0800

    IMP Website: fixed typo

--- vendor/bootstrap/js/js.js
+++ vendor/bootstrap/js/js.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
     $("#login").on("submit", function(e) {
         e.preventDefault();
-        if ($("#username").val() == "admin" && $("#password").val() == "admin") {
-            window.location.href = 'index.html';
-        }
+        window.location.href = 'home.html';
     })
 })
